9d582030b63ecdfbd62904a358807bca31f6b383
yaelm34 <38508308+yaelm34@users.noreply.github.com>
2018-05-10 13:24:44 +0200

    Update CurseCard.java

--- code.etu/dominion/card/CurseCard.java
+++ code.etu/dominion/card/CurseCard.java
@@ -10,13 +10,13 @@ public abstract class CurseCard extends Card {
 			public List<CardType> getTypes() {
 		
 			List<CardType> list=super.getTypes();
-			list.add(CurseCard);
+			list.add(Curse);
 			return list;
 	}
 	
 	public String toString() {
 		
-		return "Nom de la carte: " + super.name + "/n Types de la carte: Curse Card" ;
+		return "Nom de la carte: " + super.getName() + "/n Types de la carte: Curse Card" ;
 		
 	}
 
