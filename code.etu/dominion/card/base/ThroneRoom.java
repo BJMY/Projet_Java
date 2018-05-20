824c429e40d93cf31a9f65f12123ab1c0fbc4627
yaelm34 <38508308+yaelm34@users.noreply.github.com>
2018-05-20 23:24:34 +0200

    Add files via upload

--- code.etu/dominion/card/base/ThroneRoom.java
+++ code.etu/dominion/card/base/ThroneRoom.java
@@ -13,11 +13,49 @@ public class ThroneRoom extends ActionCard {
 	
 	public ThroneRoom(){
 		
-		super("ThroneRoom",4);
+		super("Throne Room",4);
 		
 	}
 	
 	public void play(Player p){
+		
+		List<String> choices=new ArrayList();
+		
+		for(Card c: p.cardsInHand()){
+			
+			if(c.getTypes().contains(CardType.Action)){
+				
+				choices.add(c.getName());
+			}
+			
+			
+		}
+		
+		if(choices.size()!=0){
+			
+			String choix= p.choose("Saisissez le nom de la carte Action que vous souhaitez jouer 2 fois",choices,false);
+			
+			for(Card c: p.cardsInHand()){
+				
+				if(c.getName().equals(choix)){
+					
+					c.play(p);
+					System.out.println("La carte a été jouée une fois");
+					
+					c.play(p);
+					System.out.println("La carte a été jouée deux fois");
+					
+					p.addInPlay(c);
+					p.removeFromHand(c);
+					
+				}
+				
+			}
+			
+		}
+		
+		
+		
 		
 	}
 	
