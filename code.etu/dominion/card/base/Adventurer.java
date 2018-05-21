734cb0d158be434b03f12a42cee3f4b96367eb19
yaelm34 <38508308+yaelm34@users.noreply.github.com>
2018-05-21 14:23:58 +0200

    Add files via upload

--- code.etu/dominion/card/base/Adventurer.java
+++ code.etu/dominion/card/base/Adventurer.java
@@ -6,7 +6,8 @@ import dominion.card.*;
 /**
  * Carte Aventurier (Adventurer)
  * 
- * Dévoilez des cartes de votre deck jusqu'à ce que 2 cartes Trésor soient dévoilées. Ajoutez ces cartes Trésor à votre main et défaussez les autres cartes dévoilées.
+ * Dévoilez des cartes de votre deck jusqu'à ce que 2 cartes Trésor soient dévoilées.
+ *  Ajoutez ces cartes Trésor à votre main et défaussez les autres cartes dévoilées.
  */
 public class Adventurer extends ActionCard {
 	
@@ -18,6 +19,28 @@ public class Adventurer extends ActionCard {
 	
 	public void play(Player p){
 		
+		int cpt = 0;
+		
+		while (p.getDraw().size()!=0 && cpt != 2) {
+			
+			Card c=p.getDraw().get(0);
+			
+			if (p.getDraw().get(0).getTypes().contains(CardType.Treasure)) {
+				p.addInHand(p.getDraw().remove(c.getName()));
+	
+				cpt++;
+				
+			}else{
+			
+				p.addToDiscard(p.getDraw().remove(c.getName()));
+			
+				
+			}
+			
+		
+			
+			
+		}
 	}
 	
 }
