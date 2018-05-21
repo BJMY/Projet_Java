2ad60af2e3e24a47047c4d49c9f46234e310773a
Julianbourdes <julian.bourdes@gmail.com>
2018-05-21 13:27:44 +0200

    OUI

--- code.etu/dominion/card/base/Adventurer.java
+++ code.etu/dominion/card/base/Adventurer.java
@@ -6,7 +6,6 @@ import dominion.card.*;
 /**
  * Carte Aventurier (Adventurer)
  * 
- * Dévoilez des cartes de votre deck jusqu'à ce que 2 cartes Trésor soient dévoilées. Ajoutez ces cartes Trésor à votre main et défaussez les autres cartes dévoilées.
  * Dévoilez des cartes de votre deck jusqu'à ce que 2 cartes Trésor soient dévoilées.
  *  Ajoutez ces cartes Trésor à votre main et défaussez les autres cartes dévoilées.
  */
